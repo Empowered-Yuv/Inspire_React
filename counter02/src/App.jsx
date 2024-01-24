01a7e1c29537911f6ef583d9ea51abaa697351ef
Empowered-Yuv <yuvkesarwani10@gmail.com>
2024-01-24 05:54:09 +0530

    why_hooks

---
+++ counter02/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(5)
+  
+  //let counter = 5
+
+  const addValue = () => {
+    //console.log("Value Added", Math.random());
+    //counter++;
+    //console.log("Clicked", counter);
+    //Problem in ui updation | react will decide
+    //react provides some methods called hooks
+    setCounter(counter + 1)
+  }
+
+  const subValue = () => {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1>Ritika And Yuv</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value</button>
+      <br />
+      <br />
+      <button
+      onClick={subValue}
+      >Sub value</button>
+    </>
+  )
+}
+
+export default App
