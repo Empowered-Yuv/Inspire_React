1a635e822f12821c2858267f09b2149a409855be
Empowered-Yuv <yuvkesarwani10@gmail.com>
2024-01-27 05:59:48 +0530

    Password_Generator

---
+++ 05Password_Generator/src/App.jsx
@@ -0,0 +1,116 @@
+
+import { useState, useCallback, useEffect, useRef } from 'react'
+
+
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+  //api call ho jaaye ya phir kuch functionality
+
+//const passwordGenerator = useCallback(fn, dependencies in array)
+
+//ref hook to take reference
+  //useRef hook
+  const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      // char nhi aaya index value aayi hai
+      pass += str.charAt(char)
+      
+    }
+
+    setPassword(pass)
+
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  //setPassword optimize krta memoise krne cache
+//password rakhte hai toh infinite loop kyuki hamesha change hota rahega
+
+
+//window use isliye krpaa rahe kyunki client side rendering hai
+
+//nextjs mai window obj nhi hotta hai kyunki server side rendering hoti hai
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
+
+   // useEffect run karane ke liye hota hai koi bhi cherhcharh ho toh
+  return (
+    
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center m-3 text-3xl p-3'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+        />
+        <button
+        onClick={copyPasswordToClipboard}
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-blue-600'
+        >copy</button>
+        
+    </div>
+    <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+        type="range"
+        min={6}
+        max={100}
+        value={length}
+         className='cursor-pointer'
+         onChange={(e) => {setLength(e.target.value)}}
+          />
+          <label>Length: {length}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+      <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={() => {
+              setNumberAllowed((prev) => !prev);
+          }}
+      />
+      <label htmlFor="numberInput">Numbers</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+          <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                  setCharAllowed((prev) => !prev )
+              }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+      </div>
+    </div>
+</div>
+    
+  )
+}
+
+export default App
--- bgChanger_04/src/App.jsx
+++ bgChanger_04/src/App.jsx
@@ -1,44 +1,107 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("white")
+  const [color, setColor] = useState("white");
 
   return (
-    <div className="w-full h-screen duration-400
-    " style={{backgroundColor: color}}>
+    <div
+      className="w-full h-screen duration-400
+    "
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-10 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-2 shadow-lg bg-white p-4 rounded-lg">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
 
-    <div className="fixed flex flex-wrap justify-center bottom-10 inset-x-0 px-2">
-      <div className="flex flex-wrap justify-center gap-2 shadow-lg bg-white p-4 rounded-lg">
-        <button onClick={() => setColor('red')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "red"}}>Red</button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
 
-        <button onClick={() => setColor('green')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "green"}}>Green</button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
 
-        <button onClick={() => setColor('blue')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "blue"}}>Blue</button>
+          <button
+            onClick={() => setColor("black")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "black" }}
+          >
+            Black
+          </button>
 
-        <button onClick={() => setColor('black')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "black"}}>Black</button>
+          <button
+            onClick={() => setColor("purple")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "purple" }}
+          >
+            Purple
+          </button>
 
-        <button onClick={() => setColor('purple')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "purple"}}>Purple</button>
+          <button
+            onClick={() => setColor("olive")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "olive" }}
+          >
+            Olive
+          </button>
 
-        <button onClick={() => setColor('olive')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "olive"}}>Olive</button>
+          <button
+            onClick={() => setColor("gray")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "gray" }}
+          >
+            Gray
+          </button>
 
-        <button onClick={() => setColor('gray')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "gray"}}>Gray</button>
+          <button
+            onClick={() => setColor("yellow")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "yellow" }}
+          >
+            Yellow
+          </button>
 
-        <button onClick={() => setColor('yellow')} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "yellow"}}>Yellow</button>
+          <button
+            onClick={() => setColor("pink")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "pink" }}
+          >
+            Pink
+          </button>
 
-        <button onClick={() => setColor('pink')} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "pink"}}>Pink</button>
+          <button
+            onClick={() => setColor("lavender")}
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "lavender" }}
+          >
+            Lavender
+          </button>
 
-        <button onClick={() => setColor('lavender')} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "lavender"}}>Lavender</button>
-
-        <button onClick={() => setColor('brown')} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "brown"}}>Brown</button>
-
-        
-        
-
+          <button
+            onClick={() => setColor("brown")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "brown" }}
+          >
+            Brown
+          </button>
+        </div>
       </div>
     </div>
-
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
