620a2bff1cd38d50a01bd3f8f85c6076aa8a607c
Empowered-Yuv <yuvkesarwani10@gmail.com>
2024-03-02 23:59:53 +0530

    redux_toolkit

--- 12_Blog_Project/src/App.jsx
+++ 12_Blog_Project/src/App.jsx
@@ -1,15 +1,43 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from 'react-redux'
 import "./App.css";
 
-function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer} from './components/index'
+import { Outlet } from 'react-router-dom'
 
-  return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+function App() {
+  
+  const [loading, setLoading] = useState(true)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then(() => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap bg-gray-600 content-between border">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
+  
 }
 
 export default App;
---
+++ 12_Blog_Project/src/store/authSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    status: false,
+    userData: null
+}
+
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.status = true
+            state.userData = action.payload.userData
+        },
+
+        logout: (state) => {
+            state.status = false
+            state.userData = null
+        },
+    }
+})
+
+export const {login, logout} = authSlice.actions
+
+export default authSlice.reducer
+
+//reducer aur functions export krna prta h
+
+
+//postSlice bhi bana sakte hai
