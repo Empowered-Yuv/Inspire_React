9da48cda94659c34f5bf67e7c8152939afbe36cf
Empowered-Yuv <yuvkesarwani10@gmail.com>
2024-03-05 22:54:56 +0530

    updation_12

--- 07_reactRouter/src/components/Header/Header.jsx
+++ 07_reactRouter/src/components/Header/Header.jsx
@@ -35,7 +35,7 @@ export default function Header() {
                             <li>
                                 <NavLink to="/"
                                     className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-[#ff8a00]" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     {/* isActive url se detect krta hai */}
